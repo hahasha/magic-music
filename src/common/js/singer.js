6580f5f0b8066bd03ff52bd6bdad7729929fa72a
hahasha <1007892941@qq.com>
2020-06-07 23:27:45 +0800

    update

--- src/common/js/singer.js
+++
@@ -1,8 +0,0 @@
-export default class Singer {
-  constructor({id, name,mid}) {
-    this.id = id
-    this.name = name
-    this.mid = mid
-    this.avatar = `https://y.gtimg.cn/music/photo_new/T001R300x300M000${id}.jpg?max_age=2592000`
-  }
-}
